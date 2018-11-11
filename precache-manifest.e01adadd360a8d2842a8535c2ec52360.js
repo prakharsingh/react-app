c6f85cae1f871b8d5b207c4ae773c5dabbba1d0c
Prakhar Singh <prakhar.singh007@gmail.com>
2018-11-11 19:43:26 +0530

    Updates

---
+++ precache-manifest.e01adadd360a8d2842a8535c2ec52360.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "6847ebccc1dd8e94767f",
+    "url": "/react-app/static/js/main.6847ebcc.chunk.js"
+  },
+  {
+    "revision": "85efaa943b61caa7a572",
+    "url": "/react-app/static/js/1.85efaa94.chunk.js"
+  },
+  {
+    "revision": "6847ebccc1dd8e94767f",
+    "url": "/react-app/static/css/main.d30b52a3.chunk.css"
+  },
+  {
+    "revision": "85efaa943b61caa7a572",
+    "url": "/react-app/static/css/1.2df65f61.chunk.css"
+  },
+  {
+    "revision": "cc3998f4930bfa03ff2128ec7118e2c1",
+    "url": "/react-app/index.html"
+  }
+];
